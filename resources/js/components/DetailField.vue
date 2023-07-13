e9c623eb5e6530c38c4c265f76cb02b55173481d
User <you@example.com>
2023-07-13 16:12:58 +0300

    Cast boolean value as Number

--- resources/js/components/DetailField.vue
+++ resources/js/components/DetailField.vue
@@ -1,14 +1,14 @@
 <template>
     <PanelItem :index="index" :field="field">
         <template #value>
-            <span v-if="field.value === '1'">
+            <span v-if="Number(field.value) === 1">
                 <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24"
                      stroke="green" stroke-width="2">
                   <path stroke-linecap="round" stroke-linejoin="round"
                         d="M9 12l2 2 4-4m6 2a9 9 0 11-18 0 9 9 0 0118 0z"/>
                 </svg>
             </span>
-            <span v-else-if="field.value === '0'">
+            <span v-else-if="Number(field.value) === 0">
                <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="red"
                     stroke-width="2">
                   <path stroke-linecap="round" stroke-linejoin="round"
--- resources/js/components/IndexField.vue
+++ resources/js/components/IndexField.vue
@@ -1,12 +1,12 @@
 <template>
-    <span v-show="this.field.value === '1'">
+    <span v-show="Number(field.value) === 1">
         <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24"
              stroke="green" stroke-width="2">
                   <path stroke-linecap="round" stroke-linejoin="round"
                         d="M9 12l2 2 4-4m6 2a9 9 0 11-18 0 9 9 0 0118 0z"/>
                 </svg>
     </span>
-    <span v-show="this.field.value === '0'">
+    <span v-show="Number(field.value) === 0">
          <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="red"
               stroke-width="2">
                   <path stroke-linecap="round" stroke-linejoin="round"
