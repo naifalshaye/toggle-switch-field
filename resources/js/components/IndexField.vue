0257b8052ee8c49c4646d1ac2c35cdbf037cb1c3
few <you@example.com>
2023-08-07 16:54:01 +0300

    fix false icon in index page

--- resources/js/components/IndexField.vue
+++ resources/js/components/IndexField.vue
@@ -9,8 +9,8 @@
     <span v-show="Number(field.value) === 0">
          <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" width="24"
               height="24" class="inline-block text-red-500" role="presentation">
-             <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
-                   d="M9 12l2 2 4-4m6 2a9 9 0 11-18 0 9 9 0 0118 0z"></path>
+                 <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                       d="M10 14l2-2m0 0l2-2m-2 2l-2-2m2 2l2 2m7-2a9 9 0 11-18 0 9 9 0 0118 0z"></path>
          </svg>
     </span>
 </template>
